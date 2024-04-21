78f640f40c2a0d7d5606f9a568ebe336f0ded473
Belgherbi Lina <127565670+belgherbi-lina@users.noreply.github.com>
2024-04-21 12:16:46 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Style/Main.css">
+    <title>Home Page</title>
+</head>
+<body>
+    <?php include './Pages/navigation.php' ?>
+
+    <?php include './Pages/footer.php' ?>
+</body>
+</html>
