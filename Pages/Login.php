ec061f284cfa8f9eaad1b0e4db3ed884645b0c98
Bochra HARICHANE <harichanebochra@gmail.com>
2024-04-22 11:40:15 +0100

    login commit

---
+++ Pages/Login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Style/Login.css">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <label for=""></label>
+        <input type="text">
+        <label for=""></label>
+        <input type="password">
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
