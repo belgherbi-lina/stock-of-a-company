f0d4b069684b88b58e4c226d4bad2d87b54f13d2
Belgherbi-Lina <belgherbilina@gmail.com>
2024-04-26 18:28:27 +0100

    home update

--- Pages/navigation.php
+++ Pages/navigation.php
@@ -1,3 +1,64 @@
-<nav>
-       
-</nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Style/Navigation.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar" data-navbar>
+        <a href="#" class="logo"><img src="./Images/Skin_Care_Logo.png" alt=""></a>
+            <ul class="navbar-list">
+              <li class="navbar-item">
+                <a href="#" class="navbar-link">Home</a>
+              </li>
+              <li class="navbar-item">
+                <a href="#" class="navbar-link">About</a>
+              </li>
+              <li class="navbar-item">
+                <a href="#" class="navbar-link">Products</a>
+              </li>
+              <li class="navbar-item">
+                <a href="#" class="navbar-link">Shop</a>
+              </li>
+              <li class="navbar-item">
+                <a href="#" class="navbar-link">Blog</a>
+              </li>
+              <li class="navbar-item">
+                <a href="#" class="navbar-link">Contact</a>
+              </li>
+            </ul>
+            <ul class="nav-action-list">
+              <li>
+                <button class="nav-action-btn">
+                  <ion-icon name="search-outline" aria-hidden="true"></ion-icon>
+                  <span class="nav-action-text">Search</span>
+                </button>
+              </li>
+              <li>
+                <a href="./Pages/Signup.php" class="nav-action-btn" >
+                  <ion-icon name="person-outline"></ion-icon>
+                  <span class="nav-action-text">Signup</span>
+                </a>
+            </li>
+            <li>
+                <button class="nav-action-btn">
+                  <ion-icon name="heart-outline" aria-hidden="false"></ion-icon>
+                </button>
+              </li>
+              <li>
+                <button class="nav-action-btn">
+                  <ion-icon name="bag-outline" aria-hidden="true"></ion-icon>
+                </button>
+              </li>
+            </ul>
+        
+        </nav>
+        </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
